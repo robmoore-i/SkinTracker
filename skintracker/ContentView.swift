9a2a8ab49477aeb84914bd241d39a45f5bd70bc7
rob <robmoore121@gmail.com>
2021-04-25 16:38:53 +0800

    Initial Commit

---
+++ skintracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  skintracker
+//
+//  Created by Rob on 25/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ skintracker/skintrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  skintrackerApp.swift
+//  skintracker
+//
+//  Created by Rob on 25/4/21.
+//
+
+import SwiftUI
+
+@main
+struct skintrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
