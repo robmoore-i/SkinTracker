ed387fba15906e335be84d0b30f37c524e81bf1c
rob <robmoore121@gmail.com>
2021-05-07 20:21:40 +0800

    - Add a title for the Record tab

--- skintracker/record/RecordTabbedView.swift
+++ skintracker/record/RecordTabbedView.swift
@@ -17,23 +17,33 @@ struct RecordTabbedView: View {
 
     var body: some View {
         TabbedView("Record", "plus.square") {
-            Form {
-                Section {
-                    DatePicker("Date", selection: $selectedDate, displayedComponents: .date)
-                    TimeOfDayToggle(selection: $selectedTimeOfDay)
-                }
+            VStack(spacing: 0) {
+                HStack {
+                    Text("You've got this 😊")
+                            .font(.largeTitle)
+                            .fontWeight(.medium)
+                            .foregroundColor(.black)
+                    Spacer()
+                }.padding()
+                Divider()
+                Form {
+                    Section {
+                        DatePicker("Date", selection: $selectedDate, displayedComponents: .date)
+                        TimeOfDayToggle(selection: $selectedTimeOfDay)
+                    }
 
-                Section {
-                    FaceRegionSpotCountGroup(selection: $selectedSpotCounts)
-                }
+                    Section {
+                        FaceRegionSpotCountGroup(selection: $selectedSpotCounts)
+                    }
 
-                Section {
-                    HStack {
-                        Spacer()
-                        Button("Save") {
-                            recordingStorage.store(Recording(selectedDate, selectedTimeOfDay, selectedSpotCounts))
-                        }.frame(maxWidth: 100).padding(10.0)
-                        Spacer()
+                    Section {
+                        HStack {
+                            Spacer()
+                            Button("Save") {
+                                recordingStorage.store(Recording(selectedDate, selectedTimeOfDay, selectedSpotCounts))
+                            }.frame(maxWidth: 100).padding(10.0)
+                            Spacer()
+                        }
                     }
                 }
             }
