88e5056eab20ddcb81cdbddf1b385b167e3ad7b2
rob <robmoore121@gmail.com>
2021-05-11 09:45:52 +0800

    - Add a little TrendArrow

--- skintracker/visualize/FromTheStartDataView.swift
+++ skintracker/visualize/FromTheStartDataView.swift
@@ -10,14 +10,40 @@ struct FromTheStartDataView: View {
 
     var body: some View {
         HStack {
-            LineChartView(data: recordingStorage.all.map {
+            let data: [Double] = recordingStorage.all.map {
                 Double($0.totalSpotCount())
-            }.reversed(),
+            }.reversed()
+            let percentageChange = 100 * (data.last! - data.first!) / data.first!
+            LineChartView(data: data,
                     title: "From the start",
                     rateValue: nil,
                     dropShadow: false,
                     valueSpecifier: "%.0f")
+            TrendArrow(percentageChange: percentageChange).padding()
             Spacer()
+        }
+    }
+}
+
+private struct TrendArrow: View {
+    let percentageChange: Double
+
+    var body: some View {
+        if (percentageChange > 0) {
+            VStack {
+                Image(systemName: "arrowtriangle.up").foregroundColor(Color.blue)
+                Text("\(percentageChange, specifier: "%.0f")%")
+            }
+        } else if (percentageChange == 0) {
+            VStack {
+                Image(systemName: "ellipsis").foregroundColor(Color.blue)
+                Text("No change")
+            }
+        } else {
+            VStack {
+                Image(systemName: "arrowtriangle.down").foregroundColor(Color.blue)
+                Text("\(percentageChange, specifier: "%.0f")%")
+            }
         }
     }
 }
