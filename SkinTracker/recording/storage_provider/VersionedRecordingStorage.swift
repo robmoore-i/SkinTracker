d995a78884407b72f19f3cfc266604a839edfd84
Rob Moore <rmoore@gradle.com>
2021-07-11 18:16:40 +0800

    - Move files for better organisation and isolation of Realm - Remove redundant import - Improve a comment

--- SkinTracker/recording/storage_provider/VersionedRecordingStorage.swift
+++ SkinTracker/recording/storage_provider/VersionedRecordingStorage.swift
@@ -2,12 +2,10 @@
 // Created by Rob on 11/7/21.
 //
 
-import Foundation
-
 protocol VersionedRecordingStorage {
     /**
-     Execute any data migrations that need to happen. After this method has been called, all recordings will be stored
-     under the latest model version.
+     Execute any data migrations that need to happen.
+     After this method has been called, all recordings should be stored under the latest model version.
      */
     func migration() throws
 
